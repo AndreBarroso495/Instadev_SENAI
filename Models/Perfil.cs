47e4894af5b9ed27dedc5adb6602e7f6a8aa6edc
Taila <taila.franco@aluno.ufabc.edu.br>
2021-02-01 10:47:52 -0300

    Projeto concluído

--- Models/Perfil.cs
+++ Models/Perfil.cs
@@ -2,6 +2,12 @@ namespace Instadev_SENAI.Models
 {
     public class Perfil
     {
+        public string Nome { get; set; }
+        
+        public string NomeUsuario { get; set; }
+        
+        public string Email { get; set; }
+        
         
     }
 }
